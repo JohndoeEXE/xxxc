8b4a2c7e2da296aa8f104361a2971a1355f7c2bf
JohndoeEXE <102329284+JohndoeEXE@users.noreply.github.com>
2025-06-02 14:36:01 -0700

    Create proxies.js

---
+++ proxies.js
@@ -0,0 +1,4 @@
+// Export your proxy list in format: "host:port:username:password"
+module.exports = [
+    "pr.oxylabs.io:7777:customer-slit1_Pklwu-cc-US-sessid-0416505364-sesstime-10:jmMw39knaMNXmCH_"
+];
